ff02db567eec9143059d528f6b0971c0a3550ccc
riccileonardo <l.lopesricci@gmail.com>
2025-06-17 20:35:05 -0400

    fix(app): alterando para o db do professor

--- backEnd/src/config/database.js
+++ backEnd/src/config/database.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2/promise')
 
 const pool = mysql.createPool({
   host: 'localhost',
-  user: 'root',
-  password: 'root', 
-  database: 'loja_magica',
+  user: 'g3',
+  password: 'G3@12345', 
+  database: 'db_3',
 })
 
 module.exports = pool
