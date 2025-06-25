013009744f54827b9226ea55d275e9b52f02c6d5
riccileonardo <l.lopesricci@gmail.com>
2025-06-24 20:21:48 -0400

    add config server teacher

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,10 +2,11 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const GROUP_NAME = 'g3'
+
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
+  base: `/${GROUP_NAME}/`,
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
